eb827a013f49def669d26794c35b59a63260b794
MaluLePoulet <168747330+MaluLePoulet@users.noreply.github.com>
2024-08-25 16:04:10 +0300

    Allure Part 2

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -1,20 +1,27 @@
 package baseEntities;
 
+import configuration.ReadProperties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.LoginPage;
 import services.BrowsersService;
 import services.WaitsService;
-
+import steps.UserSteps;
 
 public class BaseTest {
     protected WebDriver driver;
+    protected UserSteps userSteps;
     protected WaitsService waitsService;
 
     @BeforeMethod
     public void setUp() {
         driver = new BrowsersService().getDriver();
         waitsService = new WaitsService(driver);
+
+        userSteps = new UserSteps(driver);
+
+        driver.get(ReadProperties.getUrl());
     }
 
     @AfterMethod
