751eaee547070cc729a12aa44a1a5269d910491b
MaluLePoulet <168747330+MaluLePoulet@users.noreply.github.com>
2024-08-26 17:49:07 +0300

    Wrappers Classwork

---
+++ src/test/java/elements/RadioButton.java
@@ -0,0 +1,79 @@
+package elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RadioButton {
+    private List<UIElement> uiElementsList;
+    private List<String> valuesList;
+    /*
+    Реализовать поиск элемента на основании локатора
+    Реализовать метод выбора значения по порядковому номеру
+    Реализовать метод выбора значения по аттрибуту value
+    Реализовать метод выбора значения по тексту
+     */
+
+    /**
+     * Поиск элемента производится по аттрибуту name
+     *
+     * @param driver
+     * @param by
+     */
+    public RadioButton(WebDriver driver, By by) {
+        uiElementsList = new ArrayList<>();
+        for (WebElement element : driver.findElements(by)) {
+            UIElement uiElement = new UIElement(driver, element);
+            uiElementsList.add(uiElement);
+            valuesList.add(uiElement.getAttribute("value"));
+        }
+    }
+
+    public void selectByIndex(int index) {
+        if (index >= 0 && index < uiElementsList.size()) {
+            uiElementsList.get(index).click();
+        } else {
+            throw new IndexOutOfBoundsException("Индекс больше размера коллекции");
+        }
+    }
+
+    public void selectByValueSimple(String value) {
+        for (UIElement uiElement : uiElementsList) {
+            if (uiElement.getAttribute("value").equals(value)) {
+                uiElement.click();
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Элемента с таким value не существует");
+    }
+
+    public void selectByValue(String value) { // более правильный вариант
+        int index = valuesList.indexOf(value);
+        if (index >= 0) {
+            uiElementsList.get(index).click();
+        } else {
+            throw new IllegalArgumentException("Элемента с таким value не существует");
+        }
+    }
+
+    public void selectByText(String text) {
+        for (UIElement uiElement : uiElementsList) {
+            UIElement parent = uiElement.getParentElement();
+            if (parent.findElements(By.tagName("strong")).size() > 0) {
+                if (parent.findElement(By.tagName("strong")).getText().equals(text)) {
+                    uiElement.click();
+                }
+            } else {
+                if (parent.findElement(By.tagName("p")).getText().equals(text)) {
+                    uiElement.click();
+                    return;
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("Элемента с таким текстом не существует");
+    }
+}
---
+++ src/test/java/tests/RadioButtonTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import baseEntities.BaseTest;
+import configuration.ReadProperties;
+import elements.RadioButton;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import pages.projects.AddProjectPage;
+
+public class RadioButtonTest extends BaseTest {
+
+    @Test
+    public void radioButtonTest() {
+        userSteps
+                .successfulLogin(ReadProperties.username(),ReadProperties.password());
+
+        driver.get("https://aqa2704.testrail.io/index.php?/admin/projects/add");
+
+        AddProjectPage addProjectPage = new AddProjectPage(driver);
+        addProjectPage.getProjectTypeRadioButton().selectByIndex(1);
+        addProjectPage.getProjectTypeRadioButton().selectByIndex(2);
+        addProjectPage.getProjectTypeRadioButton().selectByText("Use multiple test suites to manage cases");
+
+        driver.get("https://aqa2704.testrail.io/index.php?/admin/subscription#");
+        RadioButton tmpRadioButton = new RadioButton(driver, By.cssSelector("[name='db_type']"));
+        tmpRadioButton.selectByText("MySQL Export");
+        tmpRadioButton.selectByValueSimple("2");
+    }
+}
