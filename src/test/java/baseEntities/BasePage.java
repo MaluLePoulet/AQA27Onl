3e9f25d51aa9f287da958a89a3eed3072c06abec
MaluLePoulet <168747330+MaluLePoulet@users.noreply.github.com>
2024-08-31 20:00:16 +0300

    Logger Classwork

--- src/test/java/baseEntities/BasePage.java
+++ src/test/java/baseEntities/BasePage.java
@@ -1,15 +1,21 @@
 package baseEntities;
 
 import configuration.ReadProperties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.LoadableComponent;
+import pages.LoginPage;
 import services.WaitsService;
 
 import java.time.Duration;
 
 public abstract class BasePage extends LoadableComponent<BasePage> {
+    private Logger logger = LogManager.getLogger(this);
+
+
     protected WebDriver pageDriver;
     protected final int WAIT_FOR_PAGE_READY_IN_SECONDS = 60;
 
@@ -24,9 +30,11 @@ public abstract class BasePage extends LoadableComponent<BasePage> {
     }
 
     protected void isLoaded() {
-        if (!isPageOpen()) throw new Error("Page is not loaded");
+        if (!isPageOpen()) {
+            logger.error("Page is not loaded" + this.getClass().getSimpleName());
+            throw new Error("Page is not loaded");
+        }
     }
-
 
     protected abstract By getPageIdentifier();
 
--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -1,5 +1,6 @@
 package baseEntities;
 
+import com.github.javafaker.Faker;
 import configuration.ReadProperties;
 import models.User;
 import org.openqa.selenium.WebDriver;
@@ -19,6 +20,7 @@ public class BaseTest {
     protected UserSteps userSteps;
     protected ProjectSteps projectSteps;
     protected WaitsService waitsService;
+    protected Faker faker;
 
     //Test Data
     protected User admin;
@@ -27,6 +29,7 @@ public class BaseTest {
     public void setUp(ITestContext iTestContext) {
         driver = new BrowsersService().getDriver();
         waitsService = new WaitsService(driver);
+        faker = new Faker();
         iTestContext.setAttribute("WebDriver", driver);
 
         userSteps = new UserSteps(driver);
